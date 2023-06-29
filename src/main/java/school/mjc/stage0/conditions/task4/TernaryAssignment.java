46b662bf2973b4641871f68496d34e43dbcb72fa
Aidin <bekturganovaidin@gmail.com>
2023-06-29 19:34:58 +0600

    finished task

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryAssignment {
     public void assignAndPrintBasedOnWhichBigger(int first, int second) {
+        int answer = first > second ? first : -first;
+
+        System.out.println(answer);
     }
 }
